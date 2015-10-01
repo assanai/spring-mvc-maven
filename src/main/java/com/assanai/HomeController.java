994600650d142a56f0650abb2c5331f5c1d6eb4c
amanurat <assanai17@gmail.com>
2015-10-01 11:40:09 +0700

    update request mapping parameter

--- src/main/java/com/assanai/HomeController.java
+++ src/main/java/com/assanai/HomeController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -11,12 +12,10 @@ public class HomeController {
 
     static private Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
-    @RequestMapping(name = "/home")
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home() {
 
         LOGGER.info("welcome to home controller...");
-
-
 
         return "home";
     }
